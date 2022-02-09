235d4040a01b8b9371ee4de54b8ca9a3007ed83e
jocelyninlay <inlay.j@gmail.com>
2022-02-09 10:57:26 -0500

    repo links working but not styled

--- main.js
+++ main.js
@@ -10,14 +10,39 @@ document.querySelector('#name').innerHTML = `<h2>${data.name}</h2>`
 document.querySelector('#location').innerHTML = `<p><b>Location:</b> ${data.location}</p>`
 document.querySelector('#urls').innerHTML = `<p><b>GitHub URL:</b> ${data.url}</p>`
 document.querySelector('#user-name').innerHTML = `<p><b>GitHub username:</b> ${data.login}</p>`
+// this is the long way jeanette did it
+// let username = document.createElement('h3')
+// username.innerText = data.login
+// ShadowRoot.appendChild(username)
+return data.repos_url
 })
 
-const repos = 'https://api.github.com/users/jocelyninlay/repos'
-fetch(repos)
-.then(function(response) {
-    console.log(response)
-    return response.json()
+.then(function(reposUrl){
+console.log("the new reposUrl is working")
+return fetch(reposUrl)
 })
+
+.then(function(result){
+return result.json()
+})
+
 .then(function(data){
-    document.querySelector('#repos').innerHTML = `<img src =${data.avatar_url} />`
+for (let repos of data){
+    document.querySelector('#repos').innerHTML += `<a href=${repos.url}>${repos.name}</a>` 
+}
 })
+
+
+
+
+// const repos = 'https://api.github.com/users/jocelyninlay/repos'
+// fetch(repos)
+// .then(function(response) {
+//     console.log(response)
+//     return response.json()
+// })
+// .then(function(data){
+//  console.log(data)    
+// document.querySelector('#repos').innerHTML += `<p><a href =${data.url}>${data.full_name}</a><p>`
+
+//})
